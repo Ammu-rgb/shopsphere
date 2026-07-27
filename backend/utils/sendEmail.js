a743630aa262eea5eac2a72e7d18d45fc6f34715
Amaan Khan <meamaan071@gmail.com>
2026-07-27 13:21:10 +0530

    Fix SMTP configuration

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,14 +1,19 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
+const sendEmail = async (options) => {
   await transporter.sendMail({
     from: `"ShopSphere" <${process.env.EMAIL_USER}>`,
     to: options.email,
